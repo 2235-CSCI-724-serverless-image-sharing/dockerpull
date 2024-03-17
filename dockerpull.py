7408cea47ab760710dc48f68ef955e0ec73b0cf8
Adrian Edwards <adrian@adriancedwards.com>
2024-03-17 17:28:09 -0400

    argparse boilerplate

--- dockerpull.py
+++ dockerpull.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+import argparse
 
 app = Flask(__name__)
 
@@ -9,6 +10,21 @@ def hello_world():
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='dockerpull',
+        description='Intelligent version of docker pull'
+        )
+    # parser.add_argument('filename')           # positional argument
+    # parser.add_argument('-c', '--count')      # option that takes a value
+    parser.add_argument('-d', '--background', action='store_true', help="Runs the server component of the program")
+
+    parser.add_argument('-v', '--verbose', action='store_true', help="Output more verbosely")
+    parser.add_argument('-b', '--benchmark', action='store_true', help="Whether or not to collect benchmark timing data for the run")
+
+
+    args = parser.parse_args()
+
 
     app.run(debug=True, port=5000)
 
