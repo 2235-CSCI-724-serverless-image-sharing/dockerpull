7809aaa0acedd95a6ec98d3b4d93df6ccd959c0c
Adrian Edwards <adrian@adriancedwards.com>
2024-03-18 09:26:47 -0400

    tabs

--- server.py
+++ server.py
@@ -16,8 +16,8 @@ def list_images():
 
 @app.route("/image/<id>")
 def download(id):
-    return "<p>Hello, World!</p>"
+	return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=5000)
+	app.run(debug=True, port=5000)
