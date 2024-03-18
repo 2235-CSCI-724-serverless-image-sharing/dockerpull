e3afadbc6f884c90b7815bd7c86298d8bcbd505b
Adrian Edwards <adrian@adriancedwards.com>
2024-03-18 09:26:23 -0400

    return installed image IDs

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify
+import docker
 
 app = Flask(__name__)
 
 @app.route("/")
 def list_images():
+	client = docker.from_env()
+	installed_images = client.images.list()
+	installed_image_ids = [i.id for i in installed_images]
 	# return JSON that identifies the server and lists the image/layer IDs it can send
-    return jsonify({
+	return jsonify({
 		"dockerpull_version": "0",
-		"images" : []
+		"images" : installed_image_ids
 	})
 
 @app.route("/image/<id>")
