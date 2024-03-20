85e0a1b438d0ab4b92b1200b5ee88cad04b26292
Adrian Edwards <adrian@adriancedwards.com>
2024-03-20 16:37:57 -0400

    fetch IDs from registry

--- server.py
+++ server.py
@@ -8,10 +8,14 @@ def list_images():
 	client = docker.from_env()
 	installed_images = client.images.list()
 	installed_image_ids = [i.id for i in installed_images]
+	installed_images_text = [i.tags[0] if i.tags != [] else i.id for i in installed_images]
+
+	registrydata = [client.images.get_registry_data(name) for name in installed_images_text]
+	installed_image_ids_registry = [d.id for d in registrydata]
 	# return JSON that identifies the server and lists the image/layer IDs it can send
 	return jsonify({
 		"dockerpull_version": "0",
-		"images" : installed_image_ids
+		"images" : installed_image_ids_registry
 	})
 
 @app.route("/dockerpull/image/<id>")
