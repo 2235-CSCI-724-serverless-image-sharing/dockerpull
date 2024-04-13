b590dcbc0c84084b5baaad1bd0a96d2e7405f242
Adrian Edwards <adrian@adriancedwards.com>
2024-04-12 20:35:46 -0400

    argparse

--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
 import threading
 import random
 
+import argparse
+
 class DockerHub:
     def __init__(self):
         self.counts = {}
@@ -68,11 +70,6 @@ class Node:
                     docker_hub.get(image)  # Increment the count for this image in DockerHub
                     self.images.append(image)
 
-
-def status_update(nodes):
-    for node in nodes:
-        print(node)
-
 # http://stackoverflow.com/questions/1094841/ddg#1094933
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
@@ -92,32 +89,48 @@ workloads = {
 }
 workload_names = workloads.keys()
 
-allow_cross_node_fetching = True
-instances_per_workload = 5
+parser = argparse.ArgumentParser(description='Simulate docker stuff.')
+parser.add_argument('--ipw', type=int, default=5, help='number of instances for each workload')
+parser.add_argument('--internode', action="store_true", help='whether to allow cross-node fetching')
+parser.add_argument('--nodes', type=int, default = 100, help='how many nodes to use')
+parser.add_argument('--stats', action="store_true", help='whether to print stats of the run')
+parser.add_argument('--verbose', action="store_true", help='whether to print stats of the run')
+
+
+
+args = parser.parse_args()
+
+
 
 # docker hub simulation to track downloads
 docker_hub = DockerHub()
 
 # Creating 100 Node objects
 nodes = []
-if allow_cross_node_fetching:
-    for i in range(1, 101):
+if args.internode:
+    for i in range(1, args.nodes):
         nodes.append(Node(i, all_nodes=nodes))
 else:
-    nodes = [Node(i) for i in range(1, 101)]
+    nodes = [Node(i) for i in range(1, args.nodes)]
 
-# Running the first five nodes with some image pulls
 for workload in workload_names:
-    for i in range(instances_per_workload):
+    for i in range(args.ipw):
         random_node = random.choice(nodes)
         print(f"starting workload {workload} on node {random_node.name}")
         random_node.run([workload])
 
-# Check the lookup for node1 and node5
-# print(nodes[0].lookup())  # Outputs: ['ubuntu', 'nginx', 'nodejs']
-# print(nodes[4].lookup())  # Outputs: ['ubuntu', 'nginx', 'nodejs']
-status_update(nodes)
-# Checking updated DockerHub counts after running multiple nodes
-print(docker_hub.counts)  # This will show updated counts reflecting multiple pulls by different nodes
 
-print(docker_hub.bandwidth_by_workload(workloads, format=sizeof_fmt))
+if args.stats:
+
+    # Check the lookup for node1 and node5
+    # print(nodes[0].lookup())  # Outputs: ['ubuntu', 'nginx', 'nodejs']
+    # print(nodes[4].lookup())  # Outputs: ['ubuntu', 'nginx', 'nodejs']
+    if args.verbose:
+        for node in nodes:
+            print(node)
+        
+        
+    # Checking updated DockerHub counts after running multiple nodes
+    print(docker_hub.counts)  # This will show updated counts reflecting multiple pulls by different nodes
+
+    print(docker_hub.bandwidth_by_workload(workloads, format=sizeof_fmt))
